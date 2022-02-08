fdbc4ce7383787e673e04f4f6158a25886f7362a
Alex Lougheed <alexlougheed@alexs-mbp-2.home>
2022-02-08 00:25:25 +0000

    Adding database finctionality with mySql

--- gui/Database/DatabaseController.py
+++ gui/Database/DatabaseController.py
@@ -3,5 +3,31 @@ import mysql.connector
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
-  password="Monkeyboy4"
+  password="Monkeyboy4",
+  database="sys"
 )
+
+
+
+def uploadCollision(col):
+  mycursor = mydb.cursor()
+
+  sql = "INSERT INTO Collisions (GPSLocation, TimeStamp) VALUES (%s, %s)"
+  val = [col.gps, col.timeStamp]
+
+  mycursor.execute(sql, val)
+  mydb.commit()
+  print("1 record inserted, ID:", mycursor.lastrowid)
+
+def searchForGPS(gpsData):
+
+  #gpsdata + radius calculation
+  collisionDetectionRange = gpsData 
+
+  mycursor = mydb.cursor()
+
+  sql = "SELECT * FROM Collisions WHERE GPSLocation = collisionDetectionRange"
+
+  mycursor.execute(sql)
+
+  myresult = mycursor.fetchall()
