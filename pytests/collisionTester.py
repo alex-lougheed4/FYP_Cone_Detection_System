7ac70508084928c385be98d76b13307a1c9a57ef
Alex Lougheed <alexlougheed@alexs-mbp-2.home>
2022-04-24 19:00:32 +0100

    Test editing

--- pytests/collisionTester.py
+++ pytests/collisionTester.py
@@ -6,16 +6,23 @@ from gui import main
 
 
 #test collision (with angle) and upload to database
-def collisionTest():
+def collisionTest(): 
+    angle = 35
+    main.setLongitude = 5
+    main.setLatitude = 10
+    main.collisionOccurred(angle)
     return
 
 #test enter hot area 
-def hotAreaTest():
+def hotAreaTest(): #Where to call in main?
+    main.inHotArea = TRUE
     return
 #test collision in hot area 
 def hotAreaCollisionTest():
+    main.inHotArea = TRUE
+    angle = 25
+    main.setLongitude = 5
+    main.setLatitude = 10
+    main.collisionOccurred(angle)
     return
 
-#test pre collision in hot area
-def hotAreaPreCollisionTest():
-    return
